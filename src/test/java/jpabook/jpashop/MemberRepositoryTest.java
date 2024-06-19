e737ad8c82ed6b0aaf4be03b44c0e2043e3452e6
tickling1 <allworldreview0130@gmail.com>
2024-06-19 19:55:43 +0900

    JPA-06-19

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    // 1. EntityManager 를 통한 모든 데이터 변경은 @Transactional 안에서 일어나야 한다.
+    // 2. @Transactional 이 테스트 케이스에 있으면 테스트가 끝난 후 바로 롤백을 한다.
+    // 3. @Rollback(false) 로 설정해 놓으면 롤백을 하지 않고 커밋한다.
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+    }
+}
